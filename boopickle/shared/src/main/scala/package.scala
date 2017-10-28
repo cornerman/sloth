6f8ed442742a946a8caaa3fbf193b824dfe22440
johannes karoff <johannes@karoff.net>
2017-10-28 02:54:48 +0200

    fix build for 2.11 and remove 2.10

--- boopickle/shared/src/main/scala/package.scala
+++ boopickle/shared/src/main/scala/package.scala
@@ -7,10 +7,16 @@ import sloth.server.Server
 import cats.{Functor, MonadError}
 import _root_.boopickle.Default._, java.nio.ByteBuffer
 
+import scala.util.{Failure, Success, Try}
+
 package object boopickle {
   implicit object BoopickleSerializer extends Serializer[Pickler, Pickler, ByteBuffer] {
     override def serialize[T : Pickler](arg: T): ByteBuffer = Pickle.intoBytes(arg)
-    override def deserialize[T : Pickler](arg: ByteBuffer): Either[Throwable, T] = util.Try(Unpickle[T].fromBytes(arg)).toEither
+    override def deserialize[T : Pickler](arg: ByteBuffer): Either[Throwable, T] =
+      Try(Unpickle[T].fromBytes(arg)) match {
+        case Success(arg) => Right(arg)
+        case Failure(t) => Left(t)
+      }
   }
 
   implicit class BoopickleClient(client: Client.type) {
