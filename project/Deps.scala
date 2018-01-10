6917bb54c6df3efbdae487a5ac599d8271eba9e3
johannes karoff <johannes@karoff.net>
2018-01-11 00:20:11 +0100

    update dependencies

--- project/Deps.scala
+++ project/Deps.scala
@@ -6,19 +6,19 @@ object Deps {
   import Def.{setting => dep}
 
   val scalaReflect = dep("org.scala-lang" % "scala-reflect")
-  val scalaTest = dep("org.scalatest" %%% "scalatest" % "3.0.3")
-  val shapeless = dep("com.chuusai" %%% "shapeless" % "2.3.2")
+  val scalaTest = dep("org.scalatest" %%% "scalatest" % "3.0.4")
+  val shapeless = dep("com.chuusai" %%% "shapeless" % "2.3.3")
   val cats = new {
-    val core = dep("org.typelevel" %%% "cats-core" % "1.0.0-MF")
-    val kittens = dep("org.typelevel" %%% "kittens" % "1.0.0-RC0")
+    val core = dep("org.typelevel" %%% "cats-core" % "1.0.1")
+    val kittens = dep("org.typelevel" %%% "kittens" % "1.0.0-RC2")
   }
   val boopickle = dep("io.suzaku" %%% "boopickle" % "1.2.6")
   val circe = new {
-    private val version = "0.8.0"
+    private val version = "0.9.0"
     val core = dep("io.circe" %%% "circe-core" % version)
     val generic = dep("io.circe" %%% "circe-generic" % version)
     val parser = dep("io.circe" %%% "circe-parser" % version)
     val shapes = dep("io.circe" %%% "circe-shapes" % version)
   }
-  val mycelium = dep("com.github.cornerman" % "mycelium" % "fec09a4")
+  val mycelium = dep("com.github.cornerman" % "mycelium" % "af214ac")
 }
