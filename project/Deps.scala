2ffd4c29db7a0110acc9063649ca29632abf9751
johannes karoff <johannes@karoff.net>
2018-01-11 11:09:54 +0100

    update mycelium

--- project/Deps.scala
+++ project/Deps.scala
@@ -20,5 +20,5 @@ object Deps {
     val parser = dep("io.circe" %%% "circe-parser" % version)
     val shapes = dep("io.circe" %%% "circe-shapes" % version)
   }
-  val mycelium = dep("com.github.cornerman" % "mycelium" % "af214ac")
+  val mycelium = dep("com.github.cornerman" % "mycelium" % "0015a9a")
 }
