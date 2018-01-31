1c7ea9de5fb38eafedf363914ff408e4bb84b8a3
johannes karoff <johannes@karoff.net>
2018-01-31 21:08:38 +0100

    add partialfunction to recover future in toTransport

--- sloth/shared/src/main/scala/mycelium/package.scala
+++ sloth/shared/src/main/scala/mycelium/package.scala
@@ -11,8 +11,8 @@ package object mycelium {
 
   implicit class TransportableWebsocketClient[PickleType, ErrorType](client: WebsocketClient[_, PickleType, _, ErrorType]) {
 
-    def toTransport(behaviour: SendBehaviour)(implicit ec: ExecutionContext) = new RequestTransport[PickleType, Lambda[T => Future[Either[ErrorType, T]]]] {
-      def apply(request: Request[PickleType]): Future[Either[ErrorType, PickleType]] = client.send(request.path, request.payload, behaviour)
+    def toTransport(behaviour: SendBehaviour, recover: PartialFunction[Throwable, ErrorType] = PartialFunction.empty)(implicit ec: ExecutionContext) = new RequestTransport[PickleType, Lambda[T => Future[Either[ErrorType, T]]]] {
+      def apply(request: Request[PickleType]): Future[Either[ErrorType, PickleType]] = client.send(request.path, request.payload, behaviour).recover(recover andThen Left.apply)
     }
 
     def toTransport(behaviour: SendBehaviour, onError: ErrorType => Throwable)(implicit ec: ExecutionContext) = new RequestTransport[PickleType, Future] {
