4e5b86faa401a0b4ddad9ef59ad29c671da8645a
johannes karoff <johannes@karoff.net>
2018-01-31 19:26:39 +0100

    refactor toTransport for future RequestTransport from mycelium client

--- sloth/shared/src/main/scala/mycelium/package.scala
+++ sloth/shared/src/main/scala/mycelium/package.scala
@@ -9,18 +9,17 @@ import scala.concurrent.{ExecutionContext, Future}
 
 package object mycelium {
 
-  implicit class TransportableWebsocketClient[PickleType, ErrorType](
-    client: WebsocketClient[_, PickleType, _, ErrorType]) {
+  implicit class TransportableWebsocketClient[PickleType, ErrorType](client: WebsocketClient[_, PickleType, _, ErrorType]) {
 
     def toTransport(behaviour: SendBehaviour)(implicit ec: ExecutionContext) = new RequestTransport[PickleType, Lambda[T => Future[Either[ErrorType, T]]]] {
       def apply(request: Request[PickleType]): Future[Either[ErrorType, PickleType]] = client.send(request.path, request.payload, behaviour)
     }
 
-    def toTransport(behaviour: SendBehaviour, recover: ErrorType => Future[PickleType])(implicit ec: ExecutionContext) = new RequestTransport[PickleType, Future] {
+    def toTransport(behaviour: SendBehaviour, onError: ErrorType => Throwable)(implicit ec: ExecutionContext) = new RequestTransport[PickleType, Future] {
       def apply(request: Request[PickleType]): Future[PickleType] = {
         client.send(request.path, request.payload, behaviour).flatMap {
           case Right(res) => Future.successful(res)
-          case Left(err) => recover(err)
+          case Left(err) => Future.failed(onError(err))
         }
       }
     }
