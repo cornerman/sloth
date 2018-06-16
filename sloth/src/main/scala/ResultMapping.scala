c39c81c7e6410963e7df7cd42a9ab0c83b051b38
johannes karoff <johannes@karoff.net>
2018-06-16 11:32:17 +0200

    update kind projector and use lambda for creating functionk

--- sloth/src/main/scala/ResultMapping.scala
+++ sloth/src/main/scala/ResultMapping.scala
@@ -1,12 +1,16 @@
 package sloth
 
-import cats.arrow.FunctionK
+import cats.~>
 
-trait ResultMapping[From[_], To[_]] extends FunctionK[From, To] {
+trait ResultMapping[From[_], To[_]] extends (From ~> To) {
   def apply[T](result: From[T]): To[T]
 }
 object ResultMapping {
   implicit def identityMapping[Result[_]]: ResultMapping[Result, Result] = new ResultMapping[Result, Result] {
     def apply[T](result: Result[T]): Result[T] = result
+  }
+
+  def apply[From[_], To[_]](f: From ~> To): ResultMapping[From, To] = new ResultMapping[From, To] {
+    def apply[T](result: From[T]): To[T] = f(result)
   }
 }
