2e3633e9e2e4764e3a4ae6b239cff663b9ebd7ad
johannes karoff <johannes@karoff.net>
2018-06-17 18:05:56 +0200

    fix typo

--- sloth/src/main/scala/MonadClientFailure.scala
+++ sloth/src/main/scala/MonadClientFailure.scala
@@ -3,7 +3,7 @@ package sloth
 import cats.{Monad, MonadError}
 import scala.annotation.implicitNotFound
 
-@implicitNotFound(msg = "Cannot find implicit MonadClientFailure[$Result]. Make sure there is an implicit cats.MonadError[$Result, ErrorType] and a sloth.ClientFailureConvert[ErrorType] forSome ErrorType.")
+@implicitNotFound(msg = "Cannot find implicit MonadClientFailure[$Result]. Make sure there is an implicit cats.MonadError[$Result, ErrorType] and a sloth.ClientFailureConvert[ErrorType] for some ErrorType.")
 sealed trait MonadClientFailure[Result[_]] {
   implicit def monad: MonadError[Result, _]
   def raiseError[T](failure: ClientFailure): Result[T]
