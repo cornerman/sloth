2e537e1b76b94da6a2dd61e46951add85006fd0a
johannes karoff <johannes@karoff.net>
2018-07-04 23:17:52 +0200

    refactor clienterrort

--- sloth/src/main/scala/ClientResultError.scala
+++ sloth/src/main/scala/ClientResultError.scala
@@ -4,11 +4,14 @@ import cats.MonadError
 import scala.annotation.implicitNotFound
 import cats.syntax.all._
 
-@implicitNotFound(msg = "Cannot find implicit ClientResultError[${Result}]. Make sure there is an implicit cats.MonadError[${Result}, _ >: ErrorType] and a sloth.ClientFailureConvert[ErrorType] for some ErrorType, or define a ClientResultErrorT yourself.")
-sealed trait ClientResultError[Result[_]] {
-  def mapMaybe[T, R](result: Result[T])(f: T => Either[ClientFailure, R]): Result[R]
-  def raiseError[T](failure: ClientFailure): Result[T]
+//TODO: is there a typeclass in cats for this "simpler" version of monaderror/applicativerror?
+trait ClientResultErrorT[Result[_], ErrorType] {
+  def mapMaybe[T, R](result: Result[T])(f: T => Either[ErrorType, R]): Result[R]
+  def raiseError[T](failure: ErrorType): Result[T]
 }
+
+@implicitNotFound(msg = "Cannot find implicit ClientResultError[${Result}]. Make sure there is an implicit cats.MonadError[${Result}, _ >: ErrorType] and a sloth.ClientFailureConvert[ErrorType] for some ErrorType, or define a ClientResultErrorT yourself.")
+sealed trait ClientResultError[Result[_]] extends ClientResultErrorT[Result, ClientFailure]
 object ClientResultError {
   implicit def fromMonadError[Result[_], ErrorType](implicit monad: MonadError[Result, ErrorType], converter: ClientFailureConvert[ErrorType]): ClientResultError[Result] = new ClientResultError[Result] {
     def mapMaybe[T, R](result: Result[T])(f: T => Either[ClientFailure, R]): Result[R] = result.flatMap(f andThen {
@@ -22,11 +25,4 @@ object ClientResultError {
     def mapMaybe[T, R](result: Result[T])(f: T => Either[ClientFailure, R]): Result[R] = c.mapMaybe(result)(f andThen (_.left.map(converter.convert)))
     def raiseError[T](failure: ClientFailure): Result[T] = c.raiseError[T](converter.convert(failure))
   }
-
-}
-
-//TODO: is there a typeclass in cats for this "simpler" version of monaderror/applicativerror?
-trait ClientResultErrorT[Result[_], ErrorType] {
-  def mapMaybe[T, R](result: Result[T])(f: T => Either[ErrorType, R]): Result[R]
-  def raiseError[T](failure: ErrorType): Result[T]
 }
