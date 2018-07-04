cb1612e7893516b68b8193472904e1dfca58d556
johannes karoff <johannes@karoff.net>
2018-07-04 23:35:40 +0200

    add contramapK to RequestMapping

--- sloth/src/main/scala/ResultMapping.scala
+++ sloth/src/main/scala/ResultMapping.scala
@@ -7,6 +7,9 @@ trait ResultMapping[From[_], To[_]] extends (From ~> To) { mapping =>
   final def mapK[R[_]](f: To ~> R): ResultMapping[From, R] = new ResultMapping[From, R] {
     def apply[T](result: From[T]): R[T] = f(mapping(result))
   }
+  final def contramapK[R[_]](f: R ~> From): ResultMapping[R, To] = new ResultMapping[R, To] {
+    def apply[T](result: R[T]): To[T] = mapping(f(result))
+  }
 }
 object ResultMapping {
   implicit def identityMapping[Result[_]]: ResultMapping[Result, Result] = new ResultMapping[Result, Result] {
