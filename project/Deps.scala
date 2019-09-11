ce6b11ae70befde7d738c30d27770e1ce0002498
Scala Steward <me@scala-steward.org>
2019-09-11 02:19:48 +0200

    Update kittens to 2.0.0

--- project/Deps.scala
+++ project/Deps.scala
@@ -8,7 +8,7 @@ object Deps {
   val scalaReflect = dep("org.scala-lang" % "scala-reflect")
   val scalaTest = dep("org.scalatest" %%% "scalatest" % "3.0.8")
   val cats = dep("org.typelevel" %%% "cats-core" % "1.6.1")
-  val kittens = dep("org.typelevel" %%% "kittens" % "1.2.1")
+  val kittens = dep("org.typelevel" %%% "kittens" % "2.0.0")
   val chameleon = dep("com.github.cornerman" %%% "chameleon" % "0.1.0")
   val boopickle = dep("io.suzaku" %%% "boopickle" % "1.3.1")
   val circe = new {
