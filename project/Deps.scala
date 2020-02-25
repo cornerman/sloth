a02d325d00a9ffa7ee898ed2fbd80cce1a5c2b7e
Scala Steward <me@scala-steward.org>
2020-02-25 14:22:26 +0100

    Update cats-core to 2.1.1

--- project/Deps.scala
+++ project/Deps.scala
@@ -6,7 +6,7 @@ object Deps {
   import Def.{setting => dep}
 
   val scalaReflect = dep("org.scala-lang" % "scala-reflect")
-  val cats = dep("org.typelevel" %%% "cats-core" % "2.1.0")
+  val cats = dep("org.typelevel" %%% "cats-core" % "2.1.1")
   val chameleon = dep("com.github.cornerman" %%% "chameleon" % "0.2.0")
 
   val scalaTest = dep("org.scalatest" %%% "scalatest" % "3.1.1")
