76e39ee9b3a50e8860b685dd1674fde123683ca8
Scala Steward <me@scala-steward.org>
2020-05-10 13:14:49 +0200

    Update scalatest to 3.1.2

--- project/Deps.scala
+++ project/Deps.scala
@@ -9,7 +9,7 @@ object Deps {
   val cats = dep("org.typelevel" %%% "cats-core" % "2.1.1")
   val chameleon = dep("com.github.cornerman" %%% "chameleon" % "0.2.0")
 
-  val scalaTest = dep("org.scalatest" %%% "scalatest" % "3.1.1")
+  val scalaTest = dep("org.scalatest" %%% "scalatest" % "3.1.2")
   val kittens = dep("org.typelevel" %%% "kittens" % "2.0.0")
   val boopickle = dep("io.suzaku" %%% "boopickle" % "1.3.1")
   val circe = new {
