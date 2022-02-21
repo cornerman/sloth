0a819cbf0fe1675131c551c6b6cd6adbd0318915
johannes karoff <johannes@karoff.net>
2022-02-21 14:00:35 +0100

    support tuple function for router and client handler out of the box

--- sloth/src/main/scala/ClientHandler.scala
+++ sloth/src/main/scala/ClientHandler.scala
@@ -26,8 +26,18 @@ object ClientContraHandler {
     override def contramap[A,B](fa: Kleisli[F,A,R])(f: B => A): Kleisli[F,B,R] = Kleisli(b => fa(f(b)))
   }
 
-  implicit def applicativeErrorFunc[R, F[_], ErrorType](implicit me: ApplicativeError[F, ErrorType], c: ClientFailureConvert[ErrorType]): ClientContraHandler[Function1[*, F[R]]] = new ClientContraHandler[Function1[*, F[R]]] {
+  implicit def applicativeErrorFunc[R, F[_], ErrorType](implicit me: ApplicativeError[F, ErrorType], c: ClientFailureConvert[ErrorType]): ClientContraHandler[* => F[R]] = new ClientContraHandler[* => F[R]] {
     override def raiseFailure[B](failure: ClientFailure): B => F[R] = _ => me.raiseError(c.convert(failure))
     override def contramap[A,B](fa: A => F[R])(f: B => A): B => F[R] = b => fa(f(b))
+  }
+
+  implicit def applicativeErrorKleisli2[I, R, F[_], ErrorType](implicit me: ApplicativeError[F, ErrorType], c: ClientFailureConvert[ErrorType]): ClientContraHandler[Lambda[A => Kleisli[F, (I,A), R]]] = new ClientContraHandler[Lambda[A => Kleisli[F, (I,A), R]]] {
+    override def raiseFailure[B](failure: ClientFailure): Kleisli[F,(I,B),R] = Kleisli.liftF(me.raiseError(c.convert(failure)))
+    override def contramap[A,B](fa: Kleisli[F,(I,A),R])(f: B => A): Kleisli[F,(I,B),R] = Kleisli { case (i, b) => fa((i, f(b))) }
+  }
+
+  implicit def applicativeErrorFunc2[I, R, F[_], ErrorType](implicit me: ApplicativeError[F, ErrorType], c: ClientFailureConvert[ErrorType]): ClientContraHandler[Lambda[A => (I,A) => F[R]]] = new ClientContraHandler[Lambda[A => (I,A) => F[R]]] {
+    override def raiseFailure[B](failure: ClientFailure): (I, B) => F[R] = (_,_) => me.raiseError(c.convert(failure))
+    override def contramap[A,B](fa: (I, A) => F[R])(f: B => A): (I, B) => F[R] = (i, b) => fa(i, f(b))
   }
 }
--- sloth/src/main/scala/RouterHandler.scala
+++ sloth/src/main/scala/RouterHandler.scala
@@ -16,10 +16,28 @@ object RouterContraHandler {
     }
   }
 
-  implicit def applicativeErrorFunc[R, F[_], ErrorType](implicit me: ApplicativeError[F, ErrorType], c: ServerFailureConvert[ErrorType]): RouterContraHandler[Function1[*, F[R]]] = new RouterContraHandler[Function1[*, F[R]]] {
+  implicit def applicativeErrorFunc[R, F[_], ErrorType](implicit me: ApplicativeError[F, ErrorType], c: ServerFailureConvert[ErrorType]): RouterContraHandler[* => F[R]] = new RouterContraHandler[* => F[R]] {
     override def eitherContramap[A,B](fa: A => F[R])(f: B => Either[ServerFailure, A]): B => F[R] = { b =>
       f(b) match {
         case Right(a) => fa(a)
+        case Left(error) => me.raiseError(c.convert(error))
+      }
+    }
+  }
+
+  implicit def applicativeErrorKleisli2[I, R, F[_], ErrorType](implicit me: ApplicativeError[F, ErrorType], c: ServerFailureConvert[ErrorType]): RouterContraHandler[Lambda[A => Kleisli[F, (I, A), R]]] = new RouterContraHandler[Lambda[A => Kleisli[F, (I, A), R]]] {
+    override def eitherContramap[A,B](fa: Kleisli[F, (I, A), R])(f: B => Either[ServerFailure, A]): Kleisli[F, (I, B), R] = Kleisli { case (i, b) =>
+      f(b) match {
+        case Right(a) => fa((i, a))
+        case Left(error) => me.raiseError(c.convert(error))
+      }
+    }
+  }
+
+  implicit def applicativeErrorFunc2[I, R, F[_], ErrorType](implicit me: ApplicativeError[F, ErrorType], c: ServerFailureConvert[ErrorType]): RouterContraHandler[Lambda[A => (I, A) => F[R]]] = new RouterContraHandler[Lambda[A => (I, A) => F[R]]] {
+    override def eitherContramap[A,B](fa: (I, A) => F[R])(f: B => Either[ServerFailure, A]): (I, B) => F[R] = { (i, b) =>
+      f(b) match {
+        case Right(a) => fa(i, a)
         case Left(error) => me.raiseError(c.convert(error))
       }
     }
