2da5997feb975d29977f029b99e03e81f4ee2beb
Scala Steward <scala_steward@virtuslab.com>
2023-01-07 16:06:22 +0000

    Update scalatest to 3.2.15

--- project/Deps.scala
+++ project/Deps.scala
@@ -9,7 +9,7 @@ object Deps {
   val cats = dep("org.typelevel" %%% "cats-core" % "2.9.0")
   val chameleon = dep("com.github.cornerman" %%% "chameleon" % "0.3.5")
 
-  val scalaTest = dep("org.scalatest" %%% "scalatest" % "3.2.14")
+  val scalaTest = dep("org.scalatest" %%% "scalatest" % "3.2.15")
   val kittens = dep("org.typelevel" %%% "kittens" % "3.0.0")
   val circe = new {
     private val version = "0.14.1"
