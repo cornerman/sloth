e8a24639a25aeec5b586a3b059ab80d13aa1e4ea
Scala Steward <scala_steward@virtuslab.com>
2024-06-24 12:48:37 +0000

    Update scalatest to 3.2.19

--- project/Deps.scala
+++ project/Deps.scala
@@ -5,7 +5,7 @@ import org.portablescala.sbtplatformdeps.PlatformDepsPlugin.autoImport._
 object Deps {
   import Def.{setting => dep}
 
-  val scalaTest = dep("org.scalatest" %%% "scalatest" % "3.2.18")
+  val scalaTest = dep("org.scalatest" %%% "scalatest" % "3.2.19")
 
   val scalaReflect = dep("org.scala-lang" % "scala-reflect")
   val cats = dep("org.typelevel" %%% "cats-core" % "2.12.0")
